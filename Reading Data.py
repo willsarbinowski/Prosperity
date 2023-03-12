23b7493b9cde0cda53bbf89e684f8a3eb1b0ce1f
willsarbinowski <126181502+willsarbinowski@users.noreply.github.com>
2023-03-12 15:34:14 -0400

    Update Reading Data.py

--- Reading Data.py
+++ Reading Data.py
@@ -1,10 +1,12 @@
-import pandas as pd
+import pandas as pd 
 import numpy as np
+
+fileName = '/users/sarbinowskiw/Desktop/Prospdata.csv'
 
 def read_market_data(fileName):
     df = pd.read_csv(fileName).fillna("")
     cols = df.columns.tolist()[0].split(';')
-    fin = pd.DataFrame(df.iloc[:,0].apply(lambda x: x.split(';')).values.tolist(), columns= cols)
+    fin = pd.DataFrame(df.iloc[:,0].apply(lambda x: x.split(';')).values.tolist(),columns= cols)
     to_float_cols = ['day','timestamp','bid_price_1','bid_volume_1','bid_price_2','bid_volume_2','bid_price_3','bid_volume_3','ask_price_1','ask_volume_1','ask_price_2','ask_volume_2','ask_price_3','ask_volume_3','mid_price','profit_and_loss']
     fin[to_float_cols] =  fin[to_float_cols].applymap(lambda x: float(x) if x != '' else '')
     return fin
@@ -15,4 +17,5 @@ def split_data_by_product(df):
     for prod in products:
         markets[prod]= df[df['product']==prod]
     return markets
-markets['BANANAS']['bid_price_1'].plot()
+df = read_market_data(fileName)
+markets = split_data_by_product(df)
